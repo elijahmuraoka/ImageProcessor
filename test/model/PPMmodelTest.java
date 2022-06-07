34713c65c1b97c70d47aca1931eeef9ce2494c1a
elijahmuraoka <elijahmuraoka03@gmail.com>
2022-06-07 12:08:50 -0400

    initialized PPMmodel test

--- test/model/PPMmodelTest.java
+++ test/model/PPMmodelTest.java
@@ -1,40 +1,60 @@
 package model;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * This class represents all tests needed for the PPM model.
  */
 public class PPMmodelTest {
+  String koalaName;
+  String koalaPath;
 
+  @Before
+  public void init() {
+    this.koalaName = "Koala.ppm";
+    this.koalaPath = "elijahmuraoka/Desktop/OOD/group/ImageProcessor/images/";
+
+  }
+
+  // tests the generateFileName method
   @Test
   public void generateFileName() {
   }
 
+  // tests the load method
   @Test
   public void load() {
   }
 
+  // tests the save method
   @Test
   public void save() {
   }
 
+  // tests the visualizeComponent method which visualizes
+  // individual RGB components
   @Test
-  public void visualizeComponent() {
+  public void testVisualizeComponentRGB() {
   }
 
+  // tests the visualizeComponent method which visualizes
+  // value, intensity, or luma
   @Test
   public void testVisualizeComponent() {
   }
 
+  // tests the horizontalFlip method
   @Test
   public void horizontalFlip() {
   }
 
+  // tests the verticalFlip method
   @Test
   public void verticalFlip() {
   }
 
+  // tests the brighten method
   @Test
   public void brighten() {
   }
