e5f432866984771b4e1e0898dc73aa93d35a26c5
Damian526 <58115973+Damian526@users.noreply.github.com>
2022-06-09 00:40:09 -0400

    * Implemented the Visualise command for the RGB component.

--- src/commands/VisualiseComp.java
+++ src/commands/VisualiseComp.java
@@ -31,7 +31,6 @@ public class VisualiseComp implements IPCommand {
     for (int i = 0; i < m.getHeight(); i++) {
       for (int j = 0; j < m.getWidth(); j++) {
         pixel = m.getWorkingImageData().get(i).get(j);
-        System.out.println(pixel[0]);
         vHelper(this.component, pixel);
       }
     }
@@ -45,6 +44,8 @@ public class VisualiseComp implements IPCommand {
     int b = pixel[2];
 
     //int grey = (int)((0.2989 * r) + (0.5870 * g) + (0.1140 * b));
+    int intensified = (r + g + b) / 3;
+    int lumafied = (int)((0.2126 * r) + (0.7152 * g) + (0.0722 * b));
 
     switch (s) {
       case "red":
@@ -58,6 +59,37 @@ public class VisualiseComp implements IPCommand {
       case "blue":
         pixel[0] = pixel[2];
         pixel[1] = pixel[2];
+        break;
+      case "luma":
+        pixel[0] = lumafied;
+        pixel[1] = lumafied;
+        pixel[2] = lumafied;
+        break;
+      case "intensity":
+        pixel[0] = intensified;
+        pixel[1] = intensified;
+        pixel[2] = intensified;
+        break;
+      case "value":
+        if (r > g && r > b) {
+          pixel[0] = r;
+          pixel[1] = r;
+          pixel[2] = r;
+        }
+        else if (g > r && g > b) {
+          pixel[0] = g;
+          pixel[1] = g;
+          pixel[2] = g;
+        }
+        else if (b > r && b > g) {
+          pixel[0] = b;
+          pixel[1] = b;
+          pixel[2] = b;
+
+        }
+        else {
+          // Do something or nothing
+        }
         break;
       default:
         throw new IllegalArgumentException("Invalid colour parameter: " + s);
