83f11489be4017c232c55d837eea2f959ccafbcc
elijahmuraoka <elijahmuraoka03@gmail.com>
2022-06-29 10:51:48 -0400

    add new links

--- src/view/Histogram.java
+++ src/view/Histogram.java
@@ -49,131 +49,6 @@ public class Histogram extends JPanel {
    *
    * @param g the <code>Graphics</code> object to protect
    */
-  //  @Override
-  //  protected void paintComponent(Graphics g) {
-  //    super.paintComponent(g);
-  //    int width = 256;
-  //
-  //    // the maximum area reserved to draw the actual histogram
-  //    int maxGraphArea = this.maxHeight * 3 / 4;
-  //
-  //    // calculate the maximum height
-  //    for (int i = 0; i < 256; i++) {
-  //      try {
-  //        this.maxCalculatedFrequency = Math.max(Objects.requireNonNull(this.data.get(i)),
-  //                this.maxCalculatedFrequency);
-  //      } catch (NullPointerException e) {
-  //        // skip drawing the empty rectangle on the chart
-  //      }
-  //    }
-  //
-  //    for (int i = 0; i < width; i++) {
-  //      // color background
-  //      g.setColor(this.backgroundColor);
-  //      g.fillRect(i, 0, i + 1, this.maxHeight);
-  //    }
-  //
-  //    // draw the bars on the chart
-  //    double barWidth = width / 256.0;
-  //    g.setColor(this.color);
-  //    for (Map.Entry<Integer, Integer> entry : this.data.entrySet()) {
-  //      int scale = entry.getValue() / this.maxCalculatedFrequency;
-  //      double barHeight = scale * maxGraphArea;
-  //      double baseLine = this.maxHeight + 1.0 * this.maxHeight/4 - barHeight;
-  //      g.fillRect((Integer) entry.getKey(), (int) baseLine, 1, (int) barHeight);
-  //      System.out.println(
-  //              ", BarHeight: " + barHeight
-  //                      + ", MaxHeight: " + maxGraphArea
-  //                      + ", Baseline: " + baseLine);
-  //    }
-  //    g.setColor(Color.BLACK);
-  //    g.drawLine(0, this.maxHeight / 4, width, this.maxHeight / 4);
-  //    System.out.println("Histogram Height: " + this.maxHeight + ", Width: " + width);
-  //
-  //    // draw the histogram's titles
-  //    g.setColor(Color.BLACK);
-  //    String type = "";
-  //    if (Color.RED.equals(this.color)) {
-  //      type = "Red";
-  //    } else if (Color.GREEN.equals(this.color)) {
-  //      type = "Green";
-  //    } else if (Color.BLUE.equals(this.color)) {
-  //      type = "Blue";
-  //    } else if (Color.ORANGE.equals(this.color)) {
-  //      type = "Intensity";
-  //    }
-  //    int fontSize = this.maxHeight / 8;
-  //    g.setFont(new Font("Plain", Font.BOLD, fontSize));
-  //    g.drawString(type, width / 4, this.maxHeight / 6);
-  //  }
-
-  //  @Override
-  //  protected void paintComponent(Graphics g) {
-  //    super.paintComponent(g);
-  //    this.width = 255;
-  //
-  //    // the maximum area reserved to draw the actual histogram
-  //    int maxGraphArea = this.maxHeight * 3 / 4;
-  //
-  //    // calculate the maximum height
-  //    for (int i = 0; i < 255; i++) {
-  //      try {
-  //        this.maxCalculatedFrequency = Math.max(Objects.requireNonNull(this.data.get(i)),
-  //                this.maxCalculatedFrequency);
-  //      } catch (NullPointerException e) {
-  //        // skip drawing the empty rectangle on the chart
-  //      }
-  //    }
-  //
-  //    // draw the bars on the chart
-  //    double barWidth = this.width / 255.0;
-  //    for (int i = 0; i < 255; i++) {
-  //      double barStart = i * barWidth;
-  //      double barEnd = i * barWidth + barWidth;
-  //      try {
-  //        Objects.requireNonNull(data.get(i));
-  //        double scale = 1.0 * this.maxCalculatedFrequency / maxGraphArea;
-  //        double barHeight = this.data.get(i) / scale;
-  //        double baseLine = maxGraphArea - barHeight;
-  //        System.out.println("BarStart: " + barStart
-  //                + ", BarEnd: " + barEnd
-  //                + ", BarWidth: " + barWidth
-  //                + ", BarHeight: " + barHeight
-  //                + ", MaxHeight: " + maxGraphArea
-  //                + ", Baseline: " + baseLine);
-  //        // color background
-  ////        g.setColor(this.backgroundColor);
-  ////        g.fillRect((int) barStart, 0, (int) barWidth, (int) baseLine + this.maxHeight / 4);
-  //        // draw only a background color that fills the entire column
-  //        g.setColor(this.backgroundColor);
-  //        g.fillRect((int) barStart, 0, (int) barWidth, this.maxHeight);
-  //        // color and make bars
-  //        g.setColor(this.color);
-  //        g.fillRect((int) barStart, (int) baseLine, (int) barWidth, (int) barHeight);
-  //      } catch (NullPointerException e) {
-  //        //
-  //      }
-  //    }
-  //    g.setColor(Color.BLACK);
-  //    g.drawLine(0, this.maxHeight / 4, this.width, this.maxHeight / 4);
-  //    System.out.println("Histogram Height: " + this.maxHeight + ", Width: " + this.width);
-  //
-  //    // draw the histogram's titles
-  //    g.setColor(Color.BLACK);
-  //    String type = "";
-  //    if (Color.RED.equals(this.color)) {
-  //      type = "Red";
-  //    } else if (Color.GREEN.equals(this.color)) {
-  //      type = "Green";
-  //    } else if (Color.BLUE.equals(this.color)) {
-  //      type = "Blue";
-  //    } else if (Color.ORANGE.equals(this.color)) {
-  //      type = "Intensity";
-  //    }
-  //    int fontSize = this.maxHeight / 8;
-  //    g.setFont(new Font("Plain", Font.BOLD, fontSize));
-  //    g.drawString(type, this.width / 4, this.maxHeight / 6);
-  //  }
   @Override
   protected void paintComponent(Graphics g) {
     super.paintComponent(g);
@@ -202,12 +77,12 @@ public class Histogram extends JPanel {
         double scale = 1.0 * this.data.get(i) / this.maxCalculatedFrequency;
         double barHeight = scale * maxGraphArea;
         double baseLine = this.maxHeight - barHeight;
-//        System.out.println("BarStart: " + barStart
-//                + ", BarEnd: " + barEnd
-//                + ", BarWidth: " + barWidth
-//                + ", BarHeight: " + barHeight
-//                + ", MaxHeight: " + maxGraphArea
-//                + ", Baseline: " + baseLine);
+        //        System.out.println("BarStart: " + barStart
+        //                + ", BarEnd: " + barEnd
+        //                + ", BarWidth: " + barWidth
+        //                + ", BarHeight: " + barHeight
+        //                + ", MaxHeight: " + maxGraphArea
+        //                + ", Baseline: " + baseLine);
         // draw only a background color that fills the entire column
         g.setColor(this.backgroundColor);
         g.fillRect((int) barStart, 0, (int) barWidth, this.maxHeight);
